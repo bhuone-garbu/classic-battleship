eb804f0681e008aee08059c1f88b0d42d428b346
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2019-09-19 15:44:34 +0100

    some initial prototype

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,16 @@
 const FLEETS = ['destroyer', 'submarine', 'battleship', 'carrier']
+const DEFAULT_GRID_COLOR = 'default-grid-color'
+
+
+const calculateCoordinates = (gridIndex, width) => {
+  // return x and y coordinate
+  return [ gridIndex % width, Math.floor(gridIndex / width) ]
+}
+
+const getIndexFromCoordinates = (x, y, width) => {
+  return y * width + x
+}
+
 
 class Player {
   constructor(name = 'Player1'){
@@ -28,7 +40,6 @@ class Bot extends Player{
   calculateNextAttactCoordinate(){
     return 2
   }
-
 }
 
 
@@ -43,16 +54,56 @@ class Game {
     this.bot = bot
   }
 
+  // the arrow syntax is intention to get the 'this.playerDivs' lex context
+  handleMouseOver = (e) => {
+    const currentDivIndex = this.playerDivs.indexOf(e.target)
+    const coordinates = calculateCoordinates(currentDivIndex, this.width )
+    
+    const neighbour1Index = getIndexFromCoordinates(coordinates[0], coordinates[1] - 1, this.width)
+    const neighbour2Index = getIndexFromCoordinates(coordinates[0], coordinates[1] + 1, this.width)
+
+    const neighbours = [currentDivIndex, neighbour1Index, neighbour2Index]
+    neighbours.forEach( index => {
+      if (this.isIndexValid(index)) this.playerDivs[index].classList.add('grid-hightlight')
+    })
+  }
+
+  handleMouseOut = (e) => {
+    const currentDivIndex = this.playerDivs.indexOf(e.target)
+    const coordinates = calculateCoordinates(currentDivIndex, this.width )
+    
+    const neighbour1Index = getIndexFromCoordinates(coordinates[0], coordinates[1] - 1, this.width)
+    const neighbour2Index = getIndexFromCoordinates(coordinates[0], coordinates[1] + 1, this.width)
+
+    const neighbours = [neighbour1Index, neighbour2Index]
+    neighbours.forEach( index => {
+      if (this.isIndexValid(index)) this.playerDivs[index].classList.remove('grid-hightlight')
+    })
+  }
+
+  // utiltiy method to check if the index is within the grid array
+  // coordinates should be checked before doing anything to it
+  isIndexValid(calculatedIndex){
+    return calculatedIndex >= 0 && calculatedIndex < this.width ** 2
+  }
+
   createGrid(){
     const playerGridDiv = document.querySelector('.player-grid')
     const botGridDiv = document.querySelector('.bot-grid')
 
     for (let i = 0; i < this.width ** 2; i++){
+      
       const playerDiv = document.createElement('div')
+      playerDiv.classList.add(DEFAULT_GRID_COLOR)
+      playerDiv.addEventListener('mouseover', this.handleMouseOver)
+      playerDiv.addEventListener('mouseout', this.handleMouseOut)
+
       this.playerDivs.push(playerDiv)
       playerGridDiv.appendChild(playerDiv)
       
       const botDiv = document.createElement('div')
+      botDiv.classList.add(DEFAULT_GRID_COLOR)
+
       this.botDivs.push(botDiv)
       botGridDiv.appendChild(botDiv)
     }
